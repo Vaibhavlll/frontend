4f3356a0a91acd44bb5c739ac167a0abd8b34b80
Vaibhav <139101953+Vaibhavlll@users.noreply.github.com>
2026-02-28 01:42:45 +0530

    Delete heidelAIWebsite-HEID-270-Automation/src directory

--- heidelAIWebsite-HEID-270-Automation/src/app/sign-up/sso-callback/page.tsx
+++
@@ -1,9 +0,0 @@
-import { AuthenticateWithRedirectCallback } from '@clerk/nextjs'
-
-export default function Page() {
-  return (
-    <div className="flex min-h-screen items-center justify-center">
-        <AuthenticateWithRedirectCallback afterSignInUrl="/sign-up" afterSignUpUrl="/sign-up" />
-    </div>
-  )
-}
--- heidelAIWebsite-HEID-270-Automation/src/components/LandingPage/Footer.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import StickyFooter from '@/components/LandingPage/ui/footer';
-
-const Footer = () => {
-  return <StickyFooter />;
-};
-
-export default Footer;
